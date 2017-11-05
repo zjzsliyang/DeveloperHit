2a2b3582f2f98a9d1ec11cb0844406e374846513
Yang LI <zjzsliyang@gmail.com>
2017-11-05 15:33:42 +0800

    Add Zhihu part

--- iOS/DeveloperHit/DeveloperHit/ZhihuViewController.swift
+++ iOS/DeveloperHit/DeveloperHit/ZhihuViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-@objcMembers class ZhihuViewController: UIViewController, XMLParserDelegate {
+@objcMembers class ZhihuViewController: UIViewController, XMLParserDelegate, UITableViewDelegate, UITableViewDataSource {
+  
+  let topicTableView = UITableView()
   
   var eleName = String()
   var questions: [Question] = []
+  var topic = String()
+  var nameCnt = 0
+  var urlCnt = 0
   
   dynamic var title_: String? = nil
   dynamic var url: String? = nil
@@ -26,6 +31,38 @@ import UIKit
   override func viewDidLoad() {
     super.viewDidLoad()
     requestZHihu()
+    initTopicTableView()
+    
+    navigationController?.navigationBar.topItem?.title = topic
+  }
+  
+  func initTopicTableView() {
+    topicTableView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+    topicTableView.delegate = self
+    topicTableView.dataSource = self
+    self.view.addSubview(topicTableView)
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return questions.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "TopicTableViewCell")
+    let question = questions[indexPath.row]
+    cell.textLabel?.text = question.user![0].name
+    cell.detailTextLabel?.text = question.title_
+    let upvoteLabel = UILabel(frame: CGRect(x: cell.frame.maxX - 100, y: 10, width: 100, height: 20))
+    upvoteLabel.textColor = UIColor(red: 0.24, green: 0.51, blue: 0.78, alpha: 1)
+    upvoteLabel.font = UIFont.systemFont(ofSize: 12)
+    upvoteLabel.text = "upvote: " + question.upvotes!
+    cell.addSubview(upvoteLabel)
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print(indexPath.row)
+//    TODO
   }
   
   func requestZHihu() {
@@ -73,6 +110,11 @@ import UIKit
       eleName = "answerUrl"
     case "user_url":
       eleName = "userUrl"
+    case "name":
+      if nameCnt == 0 {
+        topic = str
+        nameCnt += 1
+      }
     default: break
     }
     self.setValue(String(describing: value(forKey: eleName) ?? "") + str, forKey: eleName)
